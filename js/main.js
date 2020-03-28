0e904085ed5513a30e02f95b15a09475870f2343
jidrok <46357812+jidrok@users.noreply.github.com>
2020-03-28 17:22:38 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  let tabsItem = $('.tabs-item')
+
+  tabsItem.on('click', function(event){
+    event.preventDefault();
+    let activContent = $(this).attr('href');
+    $('.visible').toggleClass('visible');
+    $(activContent).toggleClass('visible');
+    $('.tabs-item-active').toggleClass('tabs-item-active');
+    $(this).toggleClass('tabs-item-active');
+
+  });
+
+});
